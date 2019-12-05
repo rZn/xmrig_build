12f021bb76dd784cff666de92e74891bbb51fa2e
Andreas Rosander <rzn@rzn.se>
2019-12-05 06:39:03 +0100

    Create compile_and_config.sh

---
+++ compile_and_config.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+cd ..
+rm -rf xmrig/
+git clone https://github.com/MoneroOcean/xmrig.git
+cd xmrig
+#git checkout v5.1.1
+mkdir build
+cd build
+cmake ..
+make 
+cd ..
+cd ..
