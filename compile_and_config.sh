a75bf2576676b20983af482f31f7b4318dcdb1bc
rZn <rzn@rzn.se>
2019-12-16 01:53:43 +0100

    v

--- compile_and_config.sh
+++ compile_and_config.sh
@@ -6,7 +6,7 @@ rm -rf xmrig/
 git clone https://github.com/MoneroOcean/xmrig.git
 cd xmrig
 #git checkout v5.1.1
-git checkout 5.3.0-mo1
+git checkout v5.3.0-mo1
 mkdir build
 sudo ./scripts/randomx_boost.sh script
 cd build
